de5a6342067978ef571db3a4195a076bd3c04696
LAB13_USER ON WS22 <L13Y12W22@LabHCI.net>
2026-03-07 11:12:35 +0800

    setup(backend): initial backend setup

---
+++ backend/src/main/java/edu/cit/estrera/wearisit/WearisitApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.estrera.wearisit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearisitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WearisitApplication.class, args);
+	}
+
+}
